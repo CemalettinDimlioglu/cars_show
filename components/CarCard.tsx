d20d64571459c3df82ce3f7604e9238e6137387d
CMLTTN <cemaldim@gmail.com>
2023-08-10 09:09:31 +0300

    carCard

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
 import { calculateCarRent } from "@/utils";
+import  CarDetails  from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -13,7 +14,7 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const carRent = calculateCarRent(city_mpg, year);
 
@@ -85,6 +86,10 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+      <CarDetails 
+      isOpen={isOpen}
+      closeModal={()=> setIsOpen(false)} car={car}   
+      />
     </div>
   );
 };
