139af743f3302881c30be15d0afe29a37acd5b7c
CMLTTN <cemaldim@gmail.com>
2023-08-11 12:13:12 +0300

    utils-index

--- utils/index.ts
+++ utils/index.ts
@@ -7,7 +7,7 @@ export async function fetchCars() {
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
     { headers: headers }
   );
 
@@ -29,3 +29,22 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
+
+ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+   const url = new URL("https://cdn.imagin.studio/getimage");
+   const { make, model, year } = car;
+
+   url.searchParams.append(
+     "customer", "hrjavascript-mastery")
+    //  process.env.NEXT_PUBLIC_IMAGIN_API_KEY || "" );
+  
+   url.searchParams.append("make", make);
+   url.searchParams.append("modelFamily", model.split(" ")[0]);
+   url.searchParams.append("zoomType", "fullscreen");
+   url.searchParams.append("modelYear", `${year}`);
+   // url.searchParams.append('zoomLevel', zoomLevel);
+   url.searchParams.append("angle", `${angle}`);
+
+   return `${url}`;
+ }; 
