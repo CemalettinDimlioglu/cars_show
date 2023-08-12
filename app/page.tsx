b92f9a68d2a5d08e15286b410d36b5803902f839
CMLTTN <cemaldim@gmail.com>
2023-08-12 21:15:01 +0300

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
 
-export default async function Home() {
+export default async function Home({searchParams}) {
 
-  const allCars= await fetchCars();
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturers || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length <1 || !allCars;
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+
 
   
   return (
@@ -20,8 +29,8 @@ export default async function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction}/>
           </div>
         </div>
         {!isDataEmpty ?(
